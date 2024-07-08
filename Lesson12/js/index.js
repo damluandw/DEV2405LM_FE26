70c51dee89733388d3f3a77ee4e861f507a3943b
damluandw <damluan7@gmail.com>
2024-07-08 21:10:52 +0700

    update lesson12 lam tiep trang chu

--- Lesson12/js/index.js
+++ Lesson12/js/index.js
@@ -37,3 +37,39 @@ $('#list-product').slick({
 $('#list-product  .slick-arrow').html("");
 $('#list-product  .slick-arrow.slick-next').html("<span>></span>");
 $('#list-product  .slick-arrow.slick-prev').html("<span><</span>");
+
+$('#list-doi-tac').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  arrows:false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
